85aba3b8bcfc9d681c5710e9de2eb3056aff761d
Irfan Mohammed <irfan.mohammed00@gmail.com>
2026-01-03 02:56:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function displayDate() {
     const day = String(today.getDate()).padStart(2, '0');
     const month = String(today.getMonth() + 1).padStart(2, '0');
     const year = today.getFullYear();
-    dateElement.textContent = `- ${day}-${month}-${year}`;
+    dateElement.textContent = `${day}-${month}-${year}`;
 }
 
 displayDate();
